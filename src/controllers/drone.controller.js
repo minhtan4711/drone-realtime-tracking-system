b5a8d6ddb3fbe01701b83576edc9182f266b8d01
minhtan <32776120+minhtan4711@users.noreply.github.com>
2026-02-06 15:47:56 +0700

    feature: build api drones

--- src/controllers/drone.controller.js
+++ src/controllers/drone.controller.js
@@ -1,14 +1,18 @@
 const droneService = require('../services/drone.service')
 
-function getCurrentDrones(req, res) {
-    const snapshot = droneService.getCurrentSnapshot()
-    res.json({
-        ts: snapshot.ts,
-        count: snapshot.drones.length,
-        drones: snapshot.drones,
-    })
+async function getAllDrones(req, res, next) {
+    try {
+        const drones = await droneService.getAllDrones()
+        res.json({
+            count: drones.length,
+            data: drones,
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
+
 module.exports = {
-    getCurrentDrones,
+    getAllDrones,
 }
