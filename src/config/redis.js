ae0c1365e67653097079207211a21cbdca48a8e2
minhtan <32776120+minhtan4711@users.noreply.github.com>
2026-02-06 18:03:02 +0700

    refactor: add redis to docker

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,9 @@
 const Redis = require('ioredis')
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
+    db: process.env.REDIS_DB || 0,
 })
 
 redis.on('connect', () => console.log('Connected to Redis'))
